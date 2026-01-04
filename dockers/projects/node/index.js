9da5c21c5717a107d1e1a3b6aa6f15ebdabd60d1
MARKASCHARAN <markascharan@gmail.com>
2026-01-04 22:56:31 +0530

    update

--- dockers/projects/node/index.js
+++ dockers/projects/node/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World, Charanss!');
+    res.send('Hello World, by NARENDRA!');
 })
 
 app.listen(3000);
